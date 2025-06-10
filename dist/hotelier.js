7b2d2a75c0f9fd1ca84f2941347822a25c877ff2
Muhammad Syahrul <msyahrul801@gmail.com>
2025-06-10 12:12:31 +0700

    ci(release): update release config

--- dist/hotelier.js
+++
@@ -1,7 +0,0 @@
-var n=()=>{document.querySelectorAll("[hotelier-widget='horizontal']").forEach((e)=>{let o=e.attachShadow({mode:"open"});o.innerHTML=`
-      <link href="https://cdn.jsdelivr.net/gh/Bintang-Technology-Solusindo/hotelier-widget@main/dist/hotelier.css" rel="stylesheet">
-      <div class="hotelier:bg-black">
-        <span class="counter-text">0</span>
-        <button class="counter-button">Tambah</button>
-      </div>
-    `})},t=n;t();
